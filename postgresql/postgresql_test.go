4f48cd1d0f730910658a0ba852de9be8eb0fa381
danilopineyro <danilo.pineyro@mercadolibre.com>
2022-04-11 00:07:22 -0300

    fix

--- postgresql/postgresql_test.go
+++ postgresql/postgresql_test.go
@@ -3,22 +3,22 @@ package postgresql_test
 
 import (
 	"os"
+	"pittsix/m/postgresql"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"gorm.io/driver/postgres"
 )
 
 func TestGetDatabaseConnection(t *testing.T) {
 	ass := assert.New(t)
-	err := postgres.GetDatabaseConnection()
+	err := postgresql.GetDatabaseConnection()
 	ass.NoError(err)
 }
 
 func TestGetDatabaseConnection_Missing_variable(t *testing.T) {
 	ass := assert.New(t)
 	os.Setenv("DATABASE_HOST", "")
-	err := postgres.GetDatabaseConnection()
+	err := postgresql.GetDatabaseConnection()
 	ass.NotNil(err)
 	ass.Equal(err.Error(), "DATABASE_USER must be set")
 }
