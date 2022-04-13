1cc7e3f028ab7a833abc7e00ab957095e71d4efa
danilopineyro <danilo.pineyro@mercadolibre.com>
2022-04-13 13:50:50 -0300

    awsptx

--- awsptx/aws.go
+++ awsptx/aws.go
@@ -2,23 +2,32 @@ package awsptx
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 const (
 	AWSAccessKeyID string = "AWS_ACCESS_KEY_ID"
 	AWSSecretKey   string = "AWS_SECRET_KEY"
 	AWSRegion      string = "AWS_REGION"
+	AWSBucketName  string = "AWS_BUCKET_NAME"
 )
 
-var AccessKeyID string
-var SecretAccessKey string
-var region string
+var (
+	AccessKeyID     string
+	SecretAccessKey string
+	region          string
+	awsAccessKeyID  = os.Getenv(AWSAccessKeyID)
+	awsSecretKey    = os.Getenv(AWSSecretKey)
+	awsRegion       = os.Getenv(AWSRegion)
+	awsBucketName   = os.Getenv(AWSBucketName)
+)
 
 var instance *session.Session
 
@@ -35,16 +44,13 @@ func GetInstance() *AWSS3 {
 }
 
 func ConnectAws() *session.Session {
-	AccessKeyID = os.Getenv(AWSAccessKeyID)
-	if AccessKeyID == "" {
+	if awsAccessKeyID == "" {
 		panic("awsS3 access key must be set")
 	}
-	SecretAccessKey = os.Getenv(AWSSecretKey)
-	if SecretAccessKey == "" {
+	if awsSecretKey == "" {
 		panic("awsS3 secret key must be set")
 	}
-	region = os.Getenv(AWSRegion)
-	if region == "" {
+	if awsRegion == "" {
 		panic("awsS3 region must be set")
 	}
 	sess, err := session.NewSession(
@@ -69,6 +75,13 @@ func (awsS3 *AWSS3) GetClient() *s3.S3 {
 	return s3.New(awsS3.Sess)
 }
 
+func (awsS3 *AWSS3) GetManagerUploader() *s3manager.Uploader {
+	if awsS3.Sess == nil {
+		GetInstance()
+	}
+	return s3manager.NewUploader(awsS3.Sess)
+}
+
 func (awsS3 *AWSS3) ListBuckets() {
 	result, err := awsS3.GetClient().ListBuckets(nil)
 	if err != nil {
@@ -81,6 +94,43 @@ func (awsS3 *AWSS3) ListBuckets() {
 		fmt.Printf("* %s created on %s\n",
 			aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
 	}
+}
+
+func (aswS3 *AWSS3) ListBucketItems() {
+	if awsBucketName == "" {
+		panic("bucket name must be set")
+	}
+	svc := GetInstance().GetClient()
+	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(awsBucketName)})
+	if err != nil {
+		exitErrorf("Unable to list items in bucket %q, %v", awsBucketName, err)
+	}
+
+	for _, item := range resp.Contents {
+		fmt.Println("Name:         ", *item.Key)
+		fmt.Println("Last modified:", *item.LastModified)
+		fmt.Println("Size:         ", *item.Size)
+		fmt.Println("Storage class:", *item.StorageClass)
+		fmt.Println("")
+	}
+}
+
+func (awsS3 *AWSS3) UploadFile(filename string, file io.Reader) {
+	if awsBucketName == "" {
+		panic("bucket name must be set")
+	}
+	uploader := awsS3.GetManagerUploader()
+	_, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(awsBucketName),
+		Key:    aws.String(filename),
+		Body:   file,
+	})
+	if err != nil {
+		// Print the error and exit.
+		exitErrorf("Unable to upload %q to %q, %v", filename, awsBucketName, err)
+	}
+
+	fmt.Printf("Successfully uploaded %q to %q\n", filename, awsBucketName)
 }
 
 func exitErrorf(msg string, args ...interface{}) {
--- awsptx/aws_test.go
+++ awsptx/aws_test.go
@@ -1,6 +1,8 @@
 package awsptx_test
 
 import (
+	"bufio"
+	"os"
 	"testing"
 
 	"github.com/devitdano/pittsix/awsptx"
@@ -10,5 +12,21 @@ import (
 func TestListBucket(t *testing.T) {
 	ass := assert.New(t)
 	awsptx.GetInstance().ListBuckets()
+	ass.True(true)
+}
+
+func TestListBucketItems(t *testing.T) {
+	ass := assert.New(t)
+	awsptx.GetInstance().ListBucketItems()
+	ass.True(true)
+}
+
+func TestListUploadFile(t *testing.T) {
+	ass := assert.New(t)
+	file, err := os.Open("../dano.jpg")
+	if err != nil {
+		panic(err)
+	}
+	awsptx.GetInstance().UploadFile("testfile", bufio.NewReader(file))
 	ass.True(true)
 }
